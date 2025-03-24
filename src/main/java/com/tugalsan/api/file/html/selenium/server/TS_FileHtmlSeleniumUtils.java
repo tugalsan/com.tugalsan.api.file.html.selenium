a1acc466d8739eddd0d9eb82c7860efd2e91b344
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-24 09:17:32 +0300

    .

--- src/main/java/com/tugalsan/api/file/html/selenium/server/TS_FileHtmlSeleniumUtils.java
+++ src/main/java/com/tugalsan/api/file/html/selenium/server/TS_FileHtmlSeleniumUtils.java
@@ -2,8 +2,8 @@ package com.tugalsan.api.file.html.selenium.server;
 
 import com.tugalsan.api.file.html.selenium.server.core.DriverHandlerChrome;
 import com.tugalsan.api.file.html.selenium.server.core.DriverHandlerEdge;
-import com.tugalsan.api.function.client.maythrow.checkedexceptions.TGS_FuncMTCEUtils;
-import com.tugalsan.api.function.client.maythrow.uncheckedexceptions.TGS_FuncMTUCE_OutBool_In1;
+import com.tugalsan.api.function.client.maythrowexceptions.checked.TGS_FuncMTCUtils;
+import com.tugalsan.api.function.client.maythrowexceptions.unchecked.TGS_FuncMTU_OutBool_In1;
 import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.thread.server.async.await.TS_ThreadAsyncAwait;
 import com.tugalsan.api.thread.server.async.await.TS_ThreadAsyncAwaitSingle;
@@ -50,7 +50,7 @@ public class TS_FileHtmlSeleniumUtils {
     }
 
     public static TGS_UnionExcuseVoid toPdf(Path urlPath, Path outputPath, PrintOptions printOptions) {
-        return TGS_FuncMTCEUtils.call(() -> {
+        return TGS_FuncMTCUtils.call(() -> {
             var options = new ChromeOptions();
             options.addArguments("--headless", "--disable-gpu", "--run-all-compositor-stages-before-draw", "--remote-allow-origins=*", "--kiosk-printing", "--scalingType:3", "--scaling:10");
             try (var driverHandler = new DriverHandlerChrome(options)) {
@@ -64,19 +64,19 @@ public class TS_FileHtmlSeleniumUtils {
         }, e -> TGS_UnionExcuseVoid.ofExcuse(e));
     }
 
-    public static TS_ThreadAsyncAwaitSingle<String> toHTML(TS_ThreadSyncTrigger killTrigger, TGS_Url url, Dimension scrnSize, Duration waitForPageLoad, TGS_FuncMTUCE_OutBool_In1<String> loadValidator, Duration waitForPstTolerans) {
+    public static TS_ThreadAsyncAwaitSingle<String> toHTML(TS_ThreadSyncTrigger killTrigger, TGS_Url url, Dimension scrnSize, Duration waitForPageLoad, TGS_FuncMTU_OutBool_In1<String> loadValidator, Duration waitForPstTolerans) {
         return toHTML(killTrigger, url.toString(), scrnSize, waitForPageLoad, loadValidator, waitForPstTolerans);
     }
 
-    public static TS_ThreadAsyncAwaitSingle<String> toHTML(TS_ThreadSyncTrigger killTrigger, Path urlPath, Dimension scrnSize, Duration waitForPageLoad, TGS_FuncMTUCE_OutBool_In1<String> loadValidator, Duration waitForPstTolerans) {
+    public static TS_ThreadAsyncAwaitSingle<String> toHTML(TS_ThreadSyncTrigger killTrigger, Path urlPath, Dimension scrnSize, Duration waitForPageLoad, TGS_FuncMTU_OutBool_In1<String> loadValidator, Duration waitForPstTolerans) {
         return toHTML(killTrigger, urlPath.toUri().toString(), scrnSize, waitForPageLoad, loadValidator, waitForPstTolerans);
     }
 
-    private static TS_ThreadAsyncAwaitSingle<String> toHTML(TS_ThreadSyncTrigger killTrigger, String urlStr, Dimension scrnSize, Duration _waitForPageLoad, TGS_FuncMTUCE_OutBool_In1<String> _loadValidator, Duration _waitForPstTolerans) {
+    private static TS_ThreadAsyncAwaitSingle<String> toHTML(TS_ThreadSyncTrigger killTrigger, String urlStr, Dimension scrnSize, Duration _waitForPageLoad, TGS_FuncMTU_OutBool_In1<String> _loadValidator, Duration _waitForPstTolerans) {
         var threshold = 1;
         var waitForPageLoad = _waitForPageLoad == null || _waitForPageLoad.toSeconds() < threshold ? Duration.ofSeconds(threshold) : _waitForPageLoad;
         var waitForPstTolerans = _waitForPstTolerans == null || _waitForPstTolerans.toSeconds() < threshold ? Duration.ofSeconds(threshold) : _waitForPstTolerans;
-        TGS_FuncMTUCE_OutBool_In1<String> loadValidator = _loadValidator == null ? html -> true : _loadValidator;
+        TGS_FuncMTU_OutBool_In1<String> loadValidator = _loadValidator == null ? html -> true : _loadValidator;
         var options = new EdgeOptions();
         try (var driverHandler = new DriverHandlerEdge(options)) {
             var driver = driverHandler.driver;
